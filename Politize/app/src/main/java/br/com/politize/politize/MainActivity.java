f8647b68b119c663965ab5a221259f57e7b33ff1
gabrieltkgp <gabrieltkgp@gmail.com>
2016-04-12 22:34:38 -0300

    first commit

---
+++ Politize/app/src/main/java/br/com/politize/politize/MainActivity.java
@@ -0,0 +1,44 @@
+package br.com.politize.politize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webView);
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());    //the lines of code added
+        myWebView.setWebChromeClient(new WebChromeClient()); //same as above
+
+        myWebView.loadUrl("http://www.politize.com.br");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (myWebView.canGoBack()) {
+                        myWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
